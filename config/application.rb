98e72effade4516075c19b5fd64224ea343b20db
giangleee <giangle602@gmail.com>
2023-08-14 15:12:55 +0700

    Chapter 3, 4, 5
    Remove .idea folder
    
    Chapter 5
    
    Add package
    
    Fix comment
    
    Add I18n in app helper
    
    Fix conflict
    
    Fix comment
    
    Fix bug

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module RailsTutorial
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = Settings.enabled_locales
+    config.i18n.default_locale = :en
+
+    config.time_zone = Settings.time_zone
   end
 end
